ba6f8f497f6095b923db62e91c8b7329a345fcb8
LordRobin1 <geb611018t@gmail.com>
2023-03-21 22:37:00 +0100

    implementation of bowyer-watson algorithm (not working yet)

--- Scripts/DelaunayTriangulation.cs
+++ Scripts/DelaunayTriangulation.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Delaunay {
@@ -26,7 +27,18 @@ namespace Delaunay {
             }
         }
 
+        public struct Edge {
+            public Vector3 a;
+            public Vector3 b;
+
+            public Edge(Vector3 _a, Vector3 _b) {
+                a = _a;
+                b = _b;
+            }
+        }
+
         public class Triangle {
+            #region Props
             public Vector3 a;
             public Vector3 b;
             public Vector3 c;
@@ -35,9 +47,24 @@ namespace Delaunay {
             public Vector3 bc;
             public Vector3 ca;
 
+            public Edge edge_ab;
+            public Edge edge_bc;
+            public Edge edge_ca;
+
             public readonly Vector3 mid_ab;
             public readonly Vector3 mid_bc;
             public readonly Vector3 mid_ac;
+            #endregion
+
+            public static Triangle zero {
+                get {
+                    return new Triangle(
+                        Vector3.zero,
+                        Vector3.zero,
+                        Vector3.zero
+                    ); ;
+                }
+            }
 
             public Triangle(Vector3 _a, Vector3 _b, Vector3 _c) {
                 a = _a;
@@ -47,6 +74,10 @@ namespace Delaunay {
                 ab = _b - _a;
                 bc = _c - _b;
                 ca = _a - _c;
+
+                edge_ab = new Edge(_a, _b);
+                edge_bc = new Edge(_b, _c);
+                edge_ca = new Edge(_c, _a);
 
                 mid_ab = (_a + _b) / 2;
                 mid_bc = (_b + _c) / 2;
@@ -68,7 +99,7 @@ namespace Delaunay {
                 return inside;
             }
 
-            // just for fun
+            // just for visualization
             public CircumCircle GetCircumCircle() {
                 Vector3 cross = Vector3.Cross(ab, bc);
                 Vector3 direction_1 = Vector3.Cross(ab, cross);
@@ -82,7 +113,7 @@ namespace Delaunay {
 
                 return new CircumCircle(intersection, radius);
             }
-            public Vector3 LineIntersection(Line line_1, Line line_2) {
+            Vector3 LineIntersection(Line line_1, Line line_2) {
                 // linear equation system
                 Vector3 factors = line_2.start - line_1.start;
                 Vector3 direction_1 = line_1.direction;
@@ -110,8 +141,9 @@ namespace Delaunay {
 
         void Start() {
             points = new Vector3[amount];
-            tris.Add(bigtri);
-            GeneratePoints();
+            RandomizePoints();
+
+            // testing stuff
             CircumCircle circumCircle = bigtri.GetCircumCircle();
             Debug.Log($"Circumcircle: {circumCircle.center}, {circumCircle.radius}");
         }
@@ -125,64 +157,141 @@ namespace Delaunay {
             render.SetTriangles(tris, points);
         }
 
-        public void GeneratePoints() {
-            for (int i = 0; i < amount; i++) {
+        public void RandomizePoints() {
+            points = new Vector3[amount];
+
+            for (int i = 0; i < points.Length; i++) {
                 points[i] = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), 0.0f);
             }
-            tris.RemoveRange(1, tris.Count - 1);
-            Debug.Log(tris.Count);
+            tris.Clear();
+
+            Initialize();
+
             Triangulation();
         }
 
+        void Initialize() {
+            AddSingleTri(bigtri, points[0]);
+            points = points.Skip(1).ToArray();
+        }
+
         void Triangulation() {
-            int i = 1;
             foreach (Vector3 point in points) {
-                Triangle currentTri = CheckTris(point);
-                Debug.Log($"{currentTri.a}, {currentTri.b}, {currentTri.c}\n" +
-                          $"{currentTri.mid_ab}, {currentTri.mid_bc}, {currentTri.mid_ac}");
-                // new tris
-                Triangle a = new Triangle(
-                    currentTri.a,
-                    currentTri.b,
-                    point
-                );
-                Triangle b = new Triangle(
-                    currentTri.b,
-                    currentTri.c,
-                    point
-                );
-                Triangle c = new Triangle(
-                    currentTri.c,
-                    currentTri.a,
-                    point
-                );
-                // Untested code
-                tris.Add(a);
-                tris.Add(b);
-                tris.Add(c);
-                i += 3;
+                AddVertex(point);
             }
         }
 
-        Triangle CheckTris(Vector3 p) {
-            // check with barycentric coordinates
-            for (int i = 1; i < tris.Count; i++) {
-                Vector3 a = tris[i].a;
-                Vector3 b = tris[i].b;
-                Vector3 c = tris[i].c;
+        void AddVertex(Vector3 p) {
+            List<Triangle> badTriangles = new();
 
-                double weight_1 = ((c.x - a.x) * (p.y - a.y) - (c.y - a.y) * (p.x - a.x))
-                               / ((b.y - a.y) * (c.x - a.x) - (b.x - a.x) * (c.y - a.y));
+            Triangle solo = Triangle.zero; // this should never remain Triangle.zero
 
-                double weight_2 = (p.y - a.y - weight_1 * (b.y - a.y)) / (c.y - a.y);
-
-                if (weight_1 >= 0 && weight_2 >= 0 && (weight_1 + weight_2) <= 1) {
-                    Debug.Log("in some Triangle ✔");
-                    return tris[i];
+            foreach (Triangle tri in tris) {
+                if (tri.InCircumCircle(p)) {
+                    solo = tri;
+                    badTriangles.Add(tri);
                 }
             }
-            Debug.Log($"Point {p} is not in bigTri");
-            return tris[0];
+
+            if (badTriangles.Count == 0) {
+                if (bigtri.InCircumCircle(p)) {
+                    AddSingleTri(bigtri, p);
+                    return;
+                }
+                Debug.LogError($"Point {p} not inside of any circumcircle");
+                return;
+            }
+            if (badTriangles.Count == 1) {
+                AddSingleTri(solo, p);
+                return;
+            }
+
+            List<Edge> polygon = new();
+
+            for (int i = 0; i < badTriangles.Count; i++) {
+                Triangle tri = badTriangles[i];
+                Edge ab = tri.edge_ab;
+                Edge bc = tri.edge_bc;
+                Edge ca = tri.edge_ca;
+
+                for (int j = 0; j < badTriangles.Count; j++) {
+                    if (i == j) continue;
+
+                    Triangle tri_2 = badTriangles[j];
+                    Edge ab_2 = tri_2.edge_ab;
+                    Edge bc_2 = tri_2.edge_bc;
+                    Edge ca_2 = tri_2.edge_ca;
+
+                    if (!ab.Equals(ab_2) && !ab.Equals(bc_2) && !ab.Equals(ca_2)) {
+                        polygon.Add(ab);
+                    }
+                    if (!bc.Equals(ab_2) && !bc.Equals(bc_2) && !bc.Equals(ca_2)) {
+                        polygon.Add(bc);
+                    }
+                    if (!ca.Equals(ab_2) && !ca.Equals(bc_2) && !ca.Equals(ca_2)) {
+                        polygon.Add(ca);
+                    }
+                }
+            }
+
+            foreach (Triangle tri in badTriangles) {
+                tris.Remove(tri);
+                //if(badTriangles.Contains(tri)) {
+                //}
+            }
+
+            foreach (Edge edge in polygon) {
+                // don't know if counterclockwise order of points is preserved
+                // which is important for the matrix's determinant's value
+                // this is most likely a big problem for future Roachl
+                Triangle newTri = new(edge.a, edge.b, p);
+                tris.Add(newTri);
+            }
+
+            return;
+        }
+
+        void AddSingleTri(Triangle currentTri, Vector3 point) {
+            Triangle a = new Triangle(
+                currentTri.a,
+                currentTri.b,
+                point
+            );
+            Triangle b = new Triangle(
+                currentTri.b,
+                currentTri.c,
+                point
+            );
+            Triangle c = new Triangle(
+                currentTri.c,
+                currentTri.a,
+                point
+            );
+
+            tris.Add(a);
+            tris.Add(b);
+            tris.Add(c);
+
+            tris.Remove(currentTri);
+        }
+
+        // (weight 1, weight 2, inside triangle) # unused, except the determinant thing turns out to not be working
+        (double, double, bool) RelativePosition(Triangle tri, Vector3 p) {
+            // check with barycentric coordinates
+            Vector3 a = tri.a;
+            Vector3 b = tri.b;
+            Vector3 c = tri.c;
+
+            double weight_1 = ((c.x - a.x) * (p.y - a.y) - (c.y - a.y) * (p.x - a.x))
+                            / ((b.y - a.y) * (c.x - a.x) - (b.x - a.x) * (c.y - a.y));
+
+            double weight_2 = (p.y - a.y - weight_1 * (b.y - a.y)) / (c.y - a.y);
+
+            if (weight_1 >= 0 && weight_2 >= 0 && (weight_1 + weight_2) <= 1) {
+                return (weight_1, weight_2, true);
+            }
+
+            return (weight_1, weight_2, false);
         }
     }
 }
